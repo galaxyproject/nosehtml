6b41d60d5298bf4631337cbc806e8d8f9739e91e
Nate Coraor <nate@bx.psu.edu>
2015-09-21 15:40:13 -0400

    Add a bit of description for PyPI

--- setup.py
+++ setup.py
@@ -1,8 +1,11 @@
 from setuptools import setup, find_packages
 
 setup( name="NoseHTML",
-	version="0.4.2",
-	description="""HTML Output plugin for Nose / Nosetests""",
-	packages=['nosehtml'],
-	entry_points = {'nose.plugins.0.10': [ 'nosehtml = nosehtml.plugin:NoseHTML' ] }
-	)
+       version="0.4.2",
+       description="""HTML Output plugin for Nose / Nosetests""",
+       url='https://bitbucket.org/james_taylor/nosehtml/',
+       author='James Taylor, Nate Coraor, Dave Bouvier',
+       license='MIT',
+       packages=['nosehtml'],
+       entry_points = {'nose.plugins.0.10': [ 'nosehtml = nosehtml.plugin:NoseHTML' ] }
+)
