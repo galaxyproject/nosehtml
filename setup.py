88b28e6e3a08897ce833850f8aa88906a814ceaa
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2017-07-07 20:11:41 +0100

    Add nose install requirement and increase minor version

--- setup.py
+++ setup.py
@@ -2,11 +2,12 @@ from setuptools import setup
 
 setup(
     name="NoseHTML",
-    version="0.4.2",
+    version="0.4.3",
     description="""HTML Output plugin for Nose / Nosetests""",
     url='https://bitbucket.org/james_taylor/nosehtml/',
     author='James Taylor, Nate Coraor, Dave Bouvier',
     license='MIT',
+    install_requires=['nose'],
     packages=['nosehtml'],
     entry_points={'nose.plugins.0.10': [ 'nosehtml = nosehtml.plugin:NoseHTML' ] }
 )
