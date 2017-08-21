6200e60d5828ce2be4bcc37a82717fc7e01a6e22
Nicola Soranzo <nsoranzo@tiscali.it>
2017-08-21 18:58:40 +0100

    Update project URL

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name="NoseHTML",
     version="0.4.3",
     description="""HTML Output plugin for Nose / Nosetests""",
-    url='https://bitbucket.org/james_taylor/nosehtml/',
+    url='https://github.com/galaxyproject/nosehtml',
     author='James Taylor, Nate Coraor, Dave Bouvier',
     license='MIT',
     install_requires=['nose'],
