844187187af795aac6117678b01ffd55bcdf5cb9
Nicola Soranzo <nicola.soranzo@earlham.ac.uk>
2017-10-18 17:55:14 +0100

    Add Trove classifiers, build universal wheels, increase minor version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="NoseHTML",
-    version="0.4.3",
+    version="0.4.4",
     description="""HTML Output plugin for Nose / Nosetests""",
     url='https://github.com/galaxyproject/nosehtml',
     author='James Taylor, Nate Coraor, Dave Bouvier',
@@ -11,5 +11,18 @@ setup(
     packages=['nosehtml'],
     entry_points={
         'nose.plugins.0.10': ['nosehtml = nosehtml.plugin:NoseHTML']
-    }
+    },
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development :: Testing",
+    ]
 )
